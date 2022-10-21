6bae9752913cdaab2450e00ba5d8b0e10793d9dc
vlcristi91 <vlcristi91@yahoo.com>
2022-10-21 09:54:45 +0300

    Update AcControllerState.h

--- include/AcControllerState.h
+++ include/AcControllerState.h
@@ -1,3 +1,14 @@
+///////////////////////////////////////////////////////////////////////////////
+/// @file     AcControllerState.h
+/// @ingroup  AcControllerState
+/// @author   Ausy Technologies
+/// @date     21/10/2022
+/// @brief    The AcControllerState class declaration
+
+/// @copyright Copyright (C) 2022
+/// @copyright Ausy Technologies
+/// @copyright All rights reserved
+///////////////////////////////////////////////////////////////////////////////
 #pragma once
 
 class AcController;
