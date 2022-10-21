f3276d89ad024ea8fafcc00322eecf662e11e939
vlcristi91 <vlcristi91@yahoo.com>
2022-10-21 09:55:50 +0300

    Update IAcControllerInput.h

--- include/IAcControllerInput.h
+++ include/IAcControllerInput.h
@@ -1,3 +1,14 @@
+///////////////////////////////////////////////////////////////////////////////
+/// @file     IAcControllerInput.h
+/// @ingroup  IAcControllerInput
+/// @author   Ausy Technologies
+/// @date     21/10/2022
+/// @brief    The IAcControllerInput class declaration
+
+/// @copyright Copyright (C) 2022
+/// @copyright Ausy Technologies
+/// @copyright All rights reserved
+///////////////////////////////////////////////////////////////////////////////
 #pragma once
 #include "common.h"
 
